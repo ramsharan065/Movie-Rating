2bcf7b2a212017076155ccfff6ab4ebec67f1f54
Ramsharan Shrestha <ramsharan065@yahoo.com>
2013-04-22 06:06:40 +0545

    Adding admin and user filters

--- MovieRatingSite/src/com/lf/movie/common/filters/AuthFilter.java
+++ MovieRatingSite/src/com/lf/movie/common/filters/AuthFilter.java
@@ -1,26 +1,28 @@
 package com.lf.movie.common.filters;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.lf.movie.auth.dto.User;
+
 /**
  * Servlet Filter implementation class AuthUserFilter
  */
-public class AuthUserFilter implements Filter {
+public class AuthFilter implements Filter {
 
     /**
      * Default constructor. 
      */
-    public AuthUserFilter() {
+    public AuthFilter() {
         // TODO Auto-generated constructor stub
     }
 
@@ -45,9 +47,18 @@ public class AuthUserFilter implements Filter {
 		
 		session = req.getSession(false);
 		if(session==null || session.getAttribute("user")==null){
+			//TODO 'remember me part should be placed here'
 			chain.doFilter(request, response);
+			return;
+		}
+		User user = (User)session.getAttribute("user");
+		if(user.getRole().equals("admin")){
+			//TODO it should be redirect to admin dashboard
+			System.out.println("redirecting to admin dashboard");
+			resp.sendRedirect(req.getContextPath()+"/app/home");
 		}
 		else{
+			System.out.println("redirecting to app home");
 			resp.sendRedirect(req.getContextPath()+"/app/home");
 		}
 	}
---
+++ MovieRatingSite/src/com/lf/movie/common/filters/UserFilter.java
@@ -0,0 +1,73 @@
+package com.lf.movie.common.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.lf.movie.auth.dto.User;
+
+/**
+ * Servlet Filter implementation class UserFilter
+ */
+@WebFilter("/UserFilter")
+public class UserFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public UserFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpSession session;
+
+		session = req.getSession(false);
+		if (session == null || session.getAttribute("user") == null) {
+			// TODO 'remember me part should be placed here'
+			System.out.println("Unauthorized request");
+			// TODO instead of redirecting to app home, it should be redirected
+			// to unauthorized page(you cannot access this page)
+			resp.sendRedirect(req.getContextPath() + "/app/home");
+			return;
+		}
+		User user = (User) session.getAttribute("user");
+		if (user.getRole().equals("admin") || user.getRole().equals("user")) {
+			chain.doFilter(request, response);
+		} else {
+			// TODO instead of redirecting to app home, it should be redirected
+			// to unauthorized page(you cannot access this page)
+			resp.sendRedirect(req.getContextPath() + "/app/home");
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
